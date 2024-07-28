f92ef14f3bc8be72afe0c7b9a8b00de9e200a3fe
Rhys How <rhykhow@gmail.com>
2024-07-29 00:10:47 +0100

    The display will now open in a maximized state

--- src/main/Display.java
+++ src/main/Display.java
@@ -1,15 +1,31 @@
 package main;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Display {
+    /*static GraphicsDevice device = GraphicsEnvironment
+            .getLocalGraphicsEnvironment().getScreenDevices()[0];*/
+
     public void initialise(){
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
         JFrame window = new JFrame();
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             window.setResizable(false);
             window.setTitle("Solar System");
+            window.setUndecorated(true);
             window.setLocationRelativeTo(null);
+            window.setExtendedState(JFrame.MAXIMIZED_BOTH);
             window.setVisible(true);
-            window.setSize(800, 600);
+
+            /*window.setSize(screenSize.width/2, screenSize.height/2);
+            window.setLocation(new Point(0, 0));
+            device.setFullScreenWindow(window);*/
+
+
         }
 }
