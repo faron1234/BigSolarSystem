972718efa53a9e5e0c89ecdd907c35ebdcf926e8
Faron Copley-Hitchcock <sc23fch@leeds.ac.uk>
2024-07-29 19:19:34 +0100

    beggining of key inputs
    mergeqqqqqq

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+
 public class Main {
     private static boolean buttonDown = false;
     private static boolean zoomIn = false;
@@ -8,13 +14,73 @@ public class Main {
     private static boolean slowDown = false;
     private static int[] mousePos = new int[2];
     private static final Display display = new Display();
+    private static final Planet[] planets = Planet.createPlanets();
 
     private static void events() {
+        display.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
 
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_DOWN) {
+                    zoomIn = true;
+                } else if (key == KeyEvent.VK_UP) {
+                    zoomOut = true;
+                } else if (key == KeyEvent.VK_EQUALS) {
+                    speedUp = true;
+                } else if (key == KeyEvent.VK_MINUS) {
+                    slowDown = true;
+                } else if (key == KeyEvent.VK_ESCAPE) {
+                    System.exit(0);
+                } else if (key == KeyEvent.VK_SPACE) {
+                    if (Planet.isMoving()) {
+                        Planet.stop();
+                    } else {
+                        Planet.move();
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_DOWN) {
+                    zoomIn = false;
+                } else if (key == KeyEvent.VK_UP) {
+                    zoomOut = false;
+                } else if (key == KeyEvent.VK_EQUALS) {
+                    speedUp = false;
+                } else if (key == KeyEvent.VK_MINUS) {
+                    slowDown = false;
+                }
+            }
+        });
+
+        display.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                buttonDown = true;
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                buttonDown = false;
+            }
+        });
+
+        display.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                mousePos[0] = e.getX();
+                mousePos[1] = e.getY();
+            }
+        });
     }
 
-    private static void renderPlanets(Planet planet) {
-        planet.render(display, 1.0, new double[]{100.0, 100.0});
+    private static void renderPlanets() {
+        Arrays.stream(planets).forEach(planet -> planet.render(display, 1.0, new double[]{100.0, 100.0}));
     }
 
     private static void speedChange() {
@@ -27,16 +93,14 @@ public class Main {
 
     public static void main(String[] args) {
         display.initialise();
-//        while (true) {
-//            // mouse_pos = ...
-//            // display.draw();
-//            events();
-//            scale();
-//            speedChange();
-//            renderPlanets();
-//            // display.update();
-//        }
-        Planet[] planets = Planet.createPlanets();
-        renderPlanets(planets[0]);
+        while (true) {
+            // mouse_pos = ...
+            // display.draw();
+            events();
+            scale();
+            speedChange();
+            renderPlanets();
+            // display.update();
+        }
     }
 }
--- src/main/Planet.java
+++ src/main/Planet.java
@@ -3,18 +3,18 @@ import javax.swing.*;
 import java.util.Random;
 
 public class Planet {
-    private final String name;
+    private Double angle = new Random().nextFloat() * (Math.PI * 2);
     private final double radius;
     private final double distance;
-    private final Double mass;
+    private final double mass;
+    private final double velocity = this.findVelocity();
+    private final Double[] Coords = new Double[2];
+    private final String name;
     private final Planet orbiting;
     private static boolean moving = true;
-    private Double[] Coords = new Double[2];
-    private final double velocity = this.findVelocity();
-    private Double angle = new Random().nextFloat() * (Math.PI * 2);
     private static double velocityScale = 1;
 
-    public Planet(String name, Double rad, Double dist, Double mass, Planet orbiting) {
+    public Planet(String name, double rad, double dist, double mass, Planet orbiting) {
         this.name = name;
         this.radius = rad;
         this.distance = dist;
@@ -29,13 +29,13 @@ public class Planet {
     public void render(Display display, double scale, double[] scaleCentre) {
         double scaledDist = this.distance / scale;
         double scaledRad = this.radius / scale;
-        ImageIcon image = new ImageIcon("Static/images/Sun.png");
         this.updatePos(scale, scaleCentre);
-        display.renderImage("Static/images/Sun.png");
+        display.renderImage(String.format("Static/images/%s.png", this.name));
+
     }
 
     private void updatePos(double scale, double[] scaleCentre) {
-        this.angle += moving ? this.velocity : 0;
+        this.angle += moving ? this.velocity * velocityScale : 0;
         double scaledDist = this.distance / scale;
         double xChange = Math.sin(this.angle) * scaledDist;
         double yChange = Math.cos(this.angle) * scaledDist;
@@ -88,17 +88,17 @@ public class Planet {
     }
 
     public static Planet[] createPlanets() {
-        Planet Sun = new Planet("Sun", 696340.0, 0.0, 1.989e30, null);
-        Planet Mercury = new Planet("Mercury", 2439.7, 57900000.0, 3.285e23, Sun);
-        Planet Venus = new Planet("Venus", 6051.8, 108200000.0, 4.867e24, Sun);
-        Planet Earth = new Planet("Earth", 6371.0, 149600000.0, 5.9722e24, Sun);
-        Planet Moon = new Planet("Moon", 1737.4, 384400.0, 7.3477e22, Earth);
-        Planet Mars = new Planet("Mars", 3389.5, 227900000.0, 6.39e23, Sun);
-        Planet Jupiter = new Planet("Jupiter", 69911.0, 778600000.0, 1.898e27, Sun);
-        Planet Saturn = new Planet("Saturn", 58232.0, 1433500000.0, 5.683e26, Sun);
-        Planet Uranus = new Planet("Uranus", 25362.0, 2872500000.0, 8.681e25, Sun);
-        Planet Neptune = new Planet("Neptune", 24622.0, 4495100000.0, 1.024e26, Sun);
-        Planet Pluto = new Planet("Pluto", 1188.3, 5906376272.0, 1.309e22, Sun);
+        Planet Sun     = new Planet("Sun",    696340.0,0.0,         1.989e30,  null);
+        Planet Mercury = new Planet("Mercury",2439.7,  57900000.0,  3.285e23,  Sun);
+        Planet Venus   = new Planet("Venus",  6051.8,  108200000.0, 4.867e24,  Sun);
+        Planet Earth   = new Planet("Earth",  6371.0,  149600000.0, 5.9722e24, Sun);
+        Planet Moon    = new Planet("Moon",   1737.4,  384400.0,    7.3477e22, Earth);
+        Planet Mars    = new Planet("Mars",   3389.5,  227900000.0, 6.39e23,   Sun);
+        Planet Jupiter = new Planet("Jupiter",69911.0, 778600000.0, 1.898e27,  Sun);
+        Planet Saturn  = new Planet("Saturn", 58232.0, 1433500000.0,5.683e26,  Sun);
+        Planet Uranus  = new Planet("Uranus", 25362.0, 2872500000.0,8.681e25,  Sun);
+        Planet Neptune = new Planet("Neptune",24622.0, 4495100000.0,1.024e26,  Sun);
+        Planet Pluto   = new Planet("Pluto",  1188.3,  5906376272.0,1.309e22,  Sun);
         return new Planet[]{Sun, Mercury, Venus, Earth, Moon, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto};
     }
 
